b800907ae3b4399c69650087807f3fbb929acba3
Abbas Khan <abbas.khan@safira.partners>
2023-06-07 13:01:27 +0100

    pwa conversion

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,9 +3,27 @@ import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
+
+const APP_NAME = "PWA App";
+const APP_DEFAULT_TITLE = "My Awesome PWA App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
+
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  themeColor: "#FFFFFF",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
 };
 
 const RootLayout = ({ children }) => (
